9ab0d0694e10630fac4df0e94d8e9ead9c7cdcaa
RicardoMokveld <0971051@hr.nl>
2019-11-26 19:48:05 +0100

    First commit

---
+++ image.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main() {
+    ofstream outfile;
+    outfile.open("image.ppm");
+    int nx = 200;
+    int ny = 100;
+    cout << "P3\n" << nx << " " << ny << "\n255\n";
+    outfile << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            cout << ir << " " << ig << " " << ib << "\n";
+            outfile << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    outfile.close();
+    return 0;
+}
